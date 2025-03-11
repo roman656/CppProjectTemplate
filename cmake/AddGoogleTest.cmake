9d633979a623161e11899b6d9ede441404bb6f3c
roman656 <romanstrah@mail.ru>
2025-03-11 21:50:10 +0300

    Upgrade CMake version; add FIND_PACKAGE_ARGS NAMES GTest to prefer system-installed GTest

--- cmake/AddGoogleTest.cmake
+++ cmake/AddGoogleTest.cmake
@@ -5,6 +5,7 @@ FetchContent_Declare(
         GIT_REPOSITORY https://github.com/google/googletest.git
         GIT_TAG v1.16.0
         GIT_SHALLOW ON
+        FIND_PACKAGE_ARGS NAMES GTest
 )
 
 # For Windows: Prevent overriding the parent project's compiler/linker settings
