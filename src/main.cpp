a9a3681b670f08cb5642d6bc0ff9117fab1f06e3
roman656 <romanstrah@mail.ru>
2025-03-19 00:32:46 +0300

    Tests in Actions

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 #include <SomeLibrary/SomeClass.hpp>
 
-int main(int argc, char* argv[])
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
     std::cout << "Hello from main()!\n";
 
